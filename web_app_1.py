2474f3564a06c8784d56d8360af4168885d4caa0
Anandvishnu24 <anandvishnu2331@gmail.com>
2024-07-18 22:30:41 +0530

    Update web_app_1.py

--- web_app_1.py
+++ web_app_1.py
@@ -18,8 +18,8 @@ import streamlit as st
  
 
 
-loaded_model = pickle.load(open('B:/ML/trained_model (2).sav', 'rb'))
-scaler = pickle.load(open('B:/ML/scaler.sav', 'rb'))
+loaded_model = pickle.load(open('trained_model (2).sav', 'rb'))
+scaler = pickle.load(open('scaler.sav', 'rb'))
 
 
 st.title('Diabetes Prediction using Machine Learning')
