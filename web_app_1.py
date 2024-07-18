44004afe3e01abe77bf96bb2156992ec91277034
Anandvishnu24 <anandvishnu2331@gmail.com>
2024-07-18 21:28:46 +0530

    Update web_app_1.py

--- web_app_1.py
+++ web_app_1.py
@@ -15,7 +15,7 @@ Created on Thu Jul 18 18:56:03 2024
 import numpy as np
 import pickle
 import streamlit as st
-
+pip install -r requirements.txt!
 
 loaded_model = pickle.load(open('trained_model (2).sav', 'rb'))
 scaler = pickle.load(open('scaler.sav', 'rb'))
