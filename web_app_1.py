faa31cbbebe0989399dd34f831da7d03cf412ce5
Anandvishnu24 <anandvishnu2331@gmail.com>
2024-07-18 20:49:36 +0530

    Update web_app_1.py

--- web_app_1.py
+++ web_app_1.py
@@ -15,8 +15,6 @@ Created on Thu Jul 18 18:56:03 2024
 import numpy as np
 import pickle
 import streamlit as st
-pip install scikit-learn
-
 
 
 loaded_model = pickle.load(open('trained_model (2).sav', 'rb'))
